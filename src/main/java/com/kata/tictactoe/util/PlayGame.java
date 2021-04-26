488741f2ffb333bab495b70fc32043a87f2d2d14
2021-DEV1-051 <2021dev1051@gmail.com>
2021-04-26 17:37:15 +0200

    Implement method that evaluates if cell is blank

--- src/main/java/com/kata/tictactoe/util/PlayGame.java
+++ src/main/java/com/kata/tictactoe/util/PlayGame.java
@@ -1,6 +1,7 @@
 package com.kata.tictactoe.util;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PlayGame {
 
@@ -16,6 +17,18 @@ public class PlayGame {
     public List<List<Cell>> playMove(List<List<Cell>> grid, int playedRow, int playedCol, PlayerType playerType) {
         grid.get(playedRow).get(playedCol).setPlayer(playerType.toString());
         return grid;
+    }
+
+    /**
+     * Check if a given cell is Blank.
+     *
+     * @param grid -
+     * @param playedRow -
+     * @param playedCol -
+     * @return true if the given box is Blank.
+     */
+    public boolean isCellBlank(List<List<Cell>> grid, int playedRow, int playedCol) {
+        return Objects.equals(grid.get(playedRow).get(playedCol).getPlayer(), PlayerType.BLANK.toString());
     }
 
 
--- src/test/java/com/kata/tictactoe/util/PlayGameTest.java
+++ src/test/java/com/kata/tictactoe/util/PlayGameTest.java
@@ -19,11 +19,34 @@ public class PlayGameTest {
         PlayGame playGame=new PlayGame();
         playGame.playMove(grid, playedRow, playedCol, PlayerType.O);
 
-        // Assert
+
+        // Assert grid is not null
         assertNotNull(grid);
+
+        // Assert
         assertEquals("O",grid.get(playedRow).get(playedCol).getPlayer());
     }
 
+    @Test
+    public void testIfCellBankAfterPlayerMove() {
+
+        // Initialize grid
+        int nbOfRowsAndCols=3;
+        List<List<Cell>> grid = GameInitializer.getGameGrid(nbOfRowsAndCols);
+
+        int playedRow=0;
+        int playedCol=0;
+
+        // Play a move
+        PlayGame playGame=new PlayGame();
+        playGame.playMove(grid, playedRow, playedCol,PlayerType.O);
+
+        // Assert grid is not null
+        assertNotNull(grid);
+
+        // Assert the cell is not empty
+        assertFalse(playGame.isCellBlank(grid,playedRow, playedCol));
+    }
 
 
 
